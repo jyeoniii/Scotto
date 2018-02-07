304be2cb63e283cfe676b2d21b5f3b593734ef0e
hyBlue <gusdud1500@naver.com>
2018-02-07 17:31:12 +0900

    uint to bool

--- Solidity/UtilContract/Common.sol
+++ Solidity/UtilContract/Common.sol
@@ -3,48 +3,48 @@ pragma solidity ^0.4.19;
 contract Common {
 
 
-    function maxResult(uint8 a, uint8 b, uint8 c) returns (uint8[3]){
+    function maxResult(uint a, uint b, uint c) returns (bool[3]){
 
-        uint8[3] memory winner;
+        bool[3] memory winner;
         if(a > b){
             if(b >= c)
-            winner[0] = 1; // a > b >= c
+            winner[0] = true; // a > b >= c
 
             else if(a > c)
-                winner[0] = 1; // a > c > b
+                winner[0] = true; // a > c > b
 
             else if(a < c)
-                winner[2] = 1; // c > a > b
+                winner[2] = true; // c > a > b
 
             else if( a== c){  // a == c > b
-                winner[0] = 1;
-                winner[2] = 1;
+                winner[0] = true;
+                winner[2] = true;
             }
 
         }
         else if(a < b){
             if( a >= c)
-            winner[1] = 1; //b > a >= c
+            winner[1] = true; //b > a >= c
             else if( b > c)
-            winner[1] = 1; // b > c > a
+            winner[1] = true; // b > c > a
             else if( b < c)
-            winner[2] = 1;
+            winner[2] = true;
             else if( b == c){
-                winner[1] = 1;
-                winner[2] = 1;
+                winner[1] = true;
+                winner[2] = true;
             }
         }
 
         else if(a == b){
-            if(a < c) winner[2] = 1;
+            if(a < c) winner[2] = true;
             else if( a > c) {
-                winner[0] = 1;
-                winner[1] = 1;
+                winner[0] = true;
+                winner[1] = true;
             }
             else{
-                winner[0] = 1;
-                winner[1] = 1;
-                winner[2] = 1;
+                winner[0] = true;
+                winner[1] = true;
+                winner[2] = true;
             }
         }
 
