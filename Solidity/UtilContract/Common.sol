68055fc4e78e22d9cfd46dd04317df332269f091
hyBlue <gusdud1500@naver.com>
2018-02-07 17:10:50 +0900

    max function

--- Solidity/UtilContract/Common.sol
+++ Solidity/UtilContract/Common.sol
@@ -1,19 +1,48 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.19;
 
 contract Common {
-    struct creator {
-      address addr;
-      uint tokenAmount;
-    }
+    
+    function maxResult(uint a, uint b, uint c) returns (uint8[3]){
+        uint8[3] memory winner;
 
-    function max(uint x, uint y, uint z) returns (uint, uint, uint){
-        if (x > z) {
-            if (x > y) return x;
-            else if (x == y) return (x, y);
-            else return y;
-        }else {
-            if (z > y) return z;
-            else return y;
+        if(a > b){
+            if(b >= c)
+            winner[0] = 1; // a > b >= c
+
+            else if(a > c)
+                winner[0] = 1; // a > c > b
+
+            else if(a < c)
+                winner[2] = 1; // c > a > b
+
+            else if( a== c){  // a == c > b
+                winner[0] = 1;
+                winner[2] = 1;
+            }
+
         }
+        else if(a < b){
+            if( a >= c)
+            winner[1] = 1; //b > a >= c
+            else if( b > c)
+            winner[1] = 1; // b > c > a
+            else if( b < c)
+            winner[2] = 1;
+            else if( b == c){
+                winner[1] = 1;
+                winner[2] = 1;
+            }
+        }
+
+        else if(a == b){
+            if(a < c) winner[2] = 1;
+            else if( a > c) winner[2] = 1;
+            else{
+                winner[0] = 1;
+                winner[1] = 1;
+                winner[2] = 1;
+            }
+        }
+
     }
 }
