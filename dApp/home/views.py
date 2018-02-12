516e47741b334bc7ffa48971b2aaea0d294ab80e
jyeoniii <jaykim9438@gmail.com>
2018-02-12 15:53:25 +0900

    Remove unused templates

--- dApp/home/views.py
+++ dApp/home/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-  return render(request, 'home/template.html', {})
+  return render(request, 'home/main.html', {})
