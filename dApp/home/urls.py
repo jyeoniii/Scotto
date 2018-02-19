13dc743b4d28f9224fc16739fef0162807b867eb
jyeoniii <jaykim9438@gmail.com>
2018-02-19 18:31:46 +0900

    Dynamic select dropdown

--- dApp/home/urls.py
+++ dApp/home/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.main, name='main'),
     url(r'^game/(?P<id>[0-9]+)$', views.game, name='game'),
+    url(r'^league/(?P<league>[-\w]+)/all_json_models/', views.all_json_models, name='all_json_models'),
 ]
