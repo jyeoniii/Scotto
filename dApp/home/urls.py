63c7896e962fe75a3e927d1cc73e2b117d950d24
hyBlue <gusdud1500@naver.com>
2018-02-20 11:28:41 +0900

    error fix

--- dApp/home/urls.py
+++ dApp/home/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     url(r'^$', views.main, name='main'),
     url(r'^game/(?P<id>[0-9]+)$', views.game, name='game'),
 
-    url(r'^account/$', views.account, name = 'account')
+    url(r'^account/$', views.account, name = 'account'),
 
     url(r'^league/(?P<league>[-\w]+)/all_json_models/', views.all_json_models, name='all_json_models'),
 
